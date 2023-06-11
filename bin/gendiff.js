6e6cbd9eb57e69776daed1137668a0a82a117913
NEK1FEKS <nikita-pol98@mail.ru>
2023-06-11 16:52:55 +0400

    Modified gendiff.js: +option -f, +argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1')
-
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>')
 program.parse();
