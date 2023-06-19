199c4b8ffa3b07e104d80fb8b3bbd6d1b36cb1fb
NEK1FEKS <nikita-pol98@mail.ru>
2023-06-19 19:48:30 +0400

    add tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
+
+const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
+test(genDiff, () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
+});
