23daa6460271d66f6ea0968e8da4be8477558464
NEK1FEKS <nikita-pol98@mail.ru>
2023-06-20 14:13:52 +0400

    Step4: modified test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
+import result from '../__fixtures__/result.js';
 
-const result = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
 test(genDiff, () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result());
 });
