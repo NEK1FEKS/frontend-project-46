a3623afca982baaae6f587b4735df6ce90c829ff
NEK1FEKS <nikita-pol98@mail.ru>
2023-07-28 16:39:44 +0400

    fix test and index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const genDiff = (filePath1, filePath2) => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const difference = keys.reduce((acc, key) => {
     if (obj1[key] !== obj2[key]) {
-      acc[`- ${key}`] = obj1[key];
-      acc[`+ ${key}`] = obj2[key];
+      acc[` - ${key}`] = obj1[key];
+      acc[` + ${key}`] = obj2[key];
     } else if (obj1[key] === obj2[key]) {
-      acc[`  ${key}`] = obj1[key];
+      acc[`   ${key}`] = obj1[key];
     }
     return acc;
   }, {});
